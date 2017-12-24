c4a7e404b8388870330946ea6bf282eb0dda5c9c
PJC-1 <jumpzine@gmail.com>
2017-12-24 13:46:28 -0800

    form submit SUCCESS and logging form field values

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -10,21 +10,29 @@ class PostsNew extends Component {
           className="form-control"
           {...field.input}
         />
+        {field.meta.error}
       </div>
     )
   }
 
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title"
           name="title"
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
         <Field
@@ -32,11 +40,31 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title || values.title.length < 3) {
+    errors.title = 'Enter a title that is at least 3 characters!';
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Enter some categories';
+  }
+
+  if (!values.content) {
+    errors.content = 'Enter some content please';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
