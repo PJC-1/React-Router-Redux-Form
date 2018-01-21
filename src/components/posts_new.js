ddb115b4c776e8dda29d864bea0ac095d7cf74e8
PJC-1 <jumpzine@gmail.com>
2018-01-20 23:35:57 -0800

    added some validation messages

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -24,8 +24,13 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
           component={this.renderField}
         />
       </form>
@@ -33,6 +38,26 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  // Validate the inputs from 'values'
+  if (!values.title || values.title.length < 3) {
+    errors.title = "Enter a title that is at least 3 characters";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories"
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content please'
+  }
+
+  // If errors is empty, the form is fine to submit
+  // If errors has any properties, redux form assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
